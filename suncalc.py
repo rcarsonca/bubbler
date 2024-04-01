27c3b63db120955606169a61c0c874c8d2595cbe
Randy Carson <rcarson@mac.com>
2024-04-01 07:55:04 -0400

    initial commit

---
+++ suncalc.py
@@ -0,0 +1,32 @@
+import time
+from datetime import datetime
+from datetime import date
+import pytz
+from suntime import Sun
+import schedule
+
+
+def calcsun():
+    latitude = 45.08608
+    longitude = -79.552073
+    tz_muskoka = pytz.timezone('America/Toronto')
+    sun = Sun(latitude, longitude)
+    global today_sr
+    today_sr = sun.get_sunrise_time().astimezone(tz_muskoka).strftime("%H:%M")
+    global today_ss
+    today_ss = sun.get_sunset_time().astimezone(tz_muskoka).strftime("%H:%M")
+    now = datetime.now()
+    ct = now.strftime("%H:%M")
+    cd = date.today()
+    print("Running calcsun at ", cd, ct)
+    print("new sunrise time:", today_sr)
+    print("new sunset time:", today_ss)
+
+
+### schedule to calcualte new sunrise/sunset every night
+schedule.every().hour.do(calcsun)
+
+calcsun()
+
+while True:
+    time.sleep(1)
